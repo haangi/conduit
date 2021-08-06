daa13e17ae3954fdac7e33492226ee72d05da3ca
Hajzler Angéla <angelahajzler@gmail.com>
2021-08-06 13:26:14 +0200

    update test_conduit.py

---
+++ test/test_cookies.py
@@ -0,0 +1,24 @@
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestConduitCookies(object):
+    def setup(self):
+        self.browser_options = Options()
+        self.browser_options.headless = True
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=self.browser_options)
+        self.browser.get("http://localhost:1667")
+
+    def teardown(self):
+        self.browser.quit()
+
+    def test_cookies(self):
+        accept_button = self.browser.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div')
+        assert self.browser.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div') != True
+        assert self.browser.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[1]/div') != True
+        accept_button.click()
